de1efcafc38a7d0182252f42b2e1310651ecc965
Adilet Toksobaev <adilet.toksobaev@gmail.com>
2022-05-19 21:08:46 +0600

    adaptive version

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,44 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
+import $ from "jquery";
+// import '~/app/libs/wow/wow.min.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+$(function() {
+    setTimeout(function(){
+        var preloader = document.getElementById('page-preloader');
+        if( !preloader.classList.contains('done')) {
+          preloader.classList.add('done');
+        }
+    }, 1000);
 
-	// Custom JS
+    $(".btn_mnu").click(function() {
+        $(this).toggleClass("active");
+        $(".menu-mobile__content").toggleClass("open");
+        $(".menu__bg").toggleClass("open");
+    });
+    
+    $(".menu-mobile__content li a").click(function() {
+        $(".menu-mobile__content").removeClass("open");
+        $(".menu__bg").removeClass("open");
+        $(".btn_mnu").removeClass("active");
+    });
 
-})
+    $(".menu__bg").click(function() {
+        $(".menu-mobile__content").removeClass("open");
+        $(".menu__bg").removeClass("open");
+        $(".btn_mnu").removeClass("active");
+    });
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 102) {
+           $('.header__fixed').addClass('fixed');
+        } else {
+           $('.header__fixed').removeClass('fixed');
+        }
+    });
+
+    $(".contact").click(function(e) {
+        e.preventDefault();
+        var aid = $(this).attr("href");
+        $('html,body').animate({scrollTop: $(aid).offset().top - 40},'slow');
+    });
+    
+});
